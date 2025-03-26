c021dbad5d5323204c28d821d9d2cde42159f486
MushMinasyan <minasyan1001@gmail.com>
2025-03-26 22:14:50 +0400

    Added reports page

---
+++ src/components/common/proBadge.tsx
@@ -0,0 +1,15 @@
+const ProBadge = () => (
+  <span
+    style={{
+      backgroundColor: "#9932CC",
+      color: "white",
+      padding: "2px 6px",
+      borderRadius: "10px",
+      marginLeft: "4px",
+      fontSize: "12px",
+    }}
+  >
+    🌟 Pro
+  </span>
+);
+export default ProBadge;
---
+++ src/pages/Reports.tsx
@@ -0,0 +1,10 @@
+import Report from "@/components/Reports-center/Report";
+import React from "react";
+
+export default function Reports() {
+  return (
+    <div>
+      <Report />
+    </div>
+  );
+}
