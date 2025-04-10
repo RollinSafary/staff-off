854e454bd943bca8d30b39d6925b0bf653c2c76d
MushMinasyan <minasyan1001@gmail.com>
2025-04-10 18:03:00 +0400

    fixes translation

---
+++ src/components/ReportsCenter/index.tsx
@@ -0,0 +1,6 @@
+import Report from "./components/Report";
+
+const Index = () => {
+  return <Report />;
+};
+export default Index;
--- src/pages/Reports.tsx
+++ src/pages/Reports.tsx
@@ -1,10 +1,9 @@
-import Report from "@/components/Reports-center/Report";
-import React from "react";
+import Index from "@/components/ReportsCenter";
 
 export default function Reports() {
   return (
     <div>
-      <Report />
+      <Index />
     </div>
   );
 }
